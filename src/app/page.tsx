2ec59806e189ca8de2e0d9a1bfdb73b5cff00ac1
Abby010 <abhishekmehta0712@gmail.com>
2025-07-10 16:33:37 +1000

    fix ESlint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
   const createBase = api.base.create.useMutation({
     onSuccess: () => {
       setNewBase("");
-      refetch();
+      void refetch();
     },
   });
 

0323872b684734404958e8fd626b98b27dec4357
Abby010 <abhishekmehta0712@gmail.com>
2025-07-22 01:09:46 +1000

    fix: use useParams hook for baseId in BasePage to support latest Next.js app router convention

--- src/app/base/[baseId]/page.tsx
+++ src/app/base/[baseId]/page.tsx
@@ -4,7 +4,6 @@ import TableWorkspace from "../../_components/table/TableWorkspace";
 import { useParams } from "next/navigation";
 
 export default function BasePage() {
-  const params = useParams();
-  const baseId = typeof params.baseId === "string" ? params.baseId : Array.isArray(params.baseId) ? params.baseId[0] : "";
-  return <TableWorkspace baseId={baseId} />;
+  const params = useParams<{ baseId: string }>();
+  return <TableWorkspace baseId={params.baseId} />;
 }

6637f11c35d3ec5f4d5c25c9f98a6f244af9e0d5
Abby010 <abhishekmehta0712@gmail.com>
2025-07-23 08:55:26 +1000

    feat: add AUTH_TRUST_HOST and AUTH_URL environment variables

--- src/env.js
+++ src/env.js
@@ -8,6 +8,9 @@ export const env = createEnv({
         ? z.string()
         : z.string().optional(),
 
+    AUTH_TRUST_HOST: z.string().optional(),
+    AUTH_URL: z.string().url().optional(),
+
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
     NEXTAUTH_URL: z.string().url(),
@@ -25,6 +28,8 @@ export const env = createEnv({
 
   runtimeEnv: {
     AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
+    AUTH_URL: process.env.AUTH_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
